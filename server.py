3f82a5256eb2966d0d374d5b94863f780c705184
Jaap Broekhuizen <jaapz.b@gmail.com>
2014-01-27 14:55:33 +0100

    Fix PEP8

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 from app import app
 from werkzeug.debug import DebuggedApplication
 
-if app.config['DEBUG'] == True:
+if app.config['DEBUG'] is True:
     app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 if __name__ == '__main__':
