72d7590319ab7686667581ec500cdbec4a24d767
Jaap Broekhuizen <jaapz.b@gmail.com>
2014-01-27 11:31:52 +0100

    Implemented registration

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,8 +1,20 @@
 from flask.ext.wtf import Form
 from wtforms import PasswordField, TextField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(Form):
     """ Form used for the login page. """
-    username = TextField('Gebruikersnaam', validators=[DataRequired()])
+    username = TextField('E-mailadres', validators=[DataRequired(), Email()])
     password = PasswordField('Wachtwoord', validators=[DataRequired()])
+
+
+class RegisterForm(Form):
+    """ Form used for registering a new user. """
+    username = TextField('E-mailadres', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Wachtwoord', [
+        DataRequired(),
+        EqualTo('confirm', message='Passwords must match')
+    ])
+
+    confirm = PasswordField('Controle Wachtwoord')
