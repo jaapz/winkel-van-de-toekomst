c2739bc55ae1a58047987d33df1ac27627dc8a8d
Jaap Broekhuizen <jaapz.b@gmail.com>
2014-01-28 11:19:23 +0100

    Implemented adding and removing product to shoppinglist from api

--- app/shoppinglist/api.py
+++ app/shoppinglist/api.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, abort, jsonify, g, Response, json
+from flask import Blueprint, abort, jsonify, g, Response, json, request
 
-from app.shoppinglist.models import ShoppingList
+from app import db
+from app.shoppinglist.models import ShoppingList, Product
 from app.users.auth import needs_auth
 
 shoppinglist_api = Blueprint('shoppinglist_api', __name__, url_prefix='/api/shoppinglists')
+product_api = Blueprint('product_api', __name__, url_prefix='/api/products')
 
 
 @shoppinglist_api.route('/<int:id>')
@@ -25,4 +27,56 @@ def get_shoppinglist_by_id(id):
 def get_shoppinglists():
     """ Fetch all shopping lists the current user can see. """
     results = [l.to_dict() for l in g.current_user.shopping_lists]
-    return Response(json.dumps(results),  mimetype='application/json')
+    return Response(json.dumps(results), mimetype='application/json')
+
+
+@shoppinglist_api.route('/<int:id>/products', methods=['GET'])
+@needs_auth
+def get_shoppinglists_products(id):
+    """ Fetch all products for a shopping list. """
+    shopping_list = ShoppingList.query.get(id)
+    if shopping_list is None:
+        abort(404)
+
+    results = [p.to_dict() for p in shopping_list.products]
+    return Response(json.dumps(results), mimetype='application/json')
+
+
+@shoppinglist_api.route('/<int:id>/products', methods=['POST', 'DELETE'])
+@needs_auth
+def post_shoppinglists_products(id):
+    """ Fetch all products for a shopping list. """
+    shopping_list = ShoppingList.query.get(id)
+    if shopping_list is None:
+        abort(404)
+
+    data = json.loads(request.data)
+    try:
+        barcode = data['id']
+    except KeyError:
+        abort(422)
+
+    product = Product.query.get(barcode)
+    if product is None:
+        abort(404)
+
+    if request.method == 'POST':
+        shopping_list.products.append(product)
+    elif request.method == 'DELETE':
+        shopping_list.products.remove(product)
+
+    db.session.commit()
+
+    results = [p.to_dict() for p in shopping_list.products]
+    return Response(json.dumps(results), mimetype='application/json')
+
+
+@product_api.route('/<int:id>', methods=['GET'])
+@needs_auth
+def get_product(id):
+    """ Get a product by id/barcode. """
+    product = Product.query.get(id)
+    if product is None:
+        abort(404)
+
+    return jsonify(product.to_dict())
